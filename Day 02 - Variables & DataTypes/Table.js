3eb9776b4eaa59061814f64df93272cbc98da804
Suraj Maurya <surajmaurya0706@gmail.com>
2024-09-20 11:58:36 +0530

    JavaScript

---
+++ Day 02 - Variables & DataTypes/Table.js
@@ -0,0 +1,18 @@
+/* const car1 = {name:"Audi", model:"A4"}
+const car2 = {name:"Volvo", model:"XC90"}
+const car3 = {name:"Ford", model:"Fusion"}
+
+console.table([car1, car2, car3]); */
+
+
+let bike1 = {name:"hero", price:"90 thousand"}
+let bike2 = {name:"honda", price:"95 thaousand"}
+let bike3 = {name:"honda live", price:"93 thousand"}
+let bike4 = {name:"pulser", price:"1.3 lakh"}
+let bike5 = {name:"bullet", price:"2.3 lakh"}
+let bike6 = {name:"apache", price:"1.5 lakh"}
+let bike7 = {name:"zx10r", price:"2 lakh"}
+let bike8 = {name:"r15", price:"2.5 lakh"}
+let bike9 = {name:"ktm duke", price:"2.5 lakh"}
+
+console.table([bike1, bike2, bike3, bike4, bike5, bike6, bike7, bike8, bike9]);
