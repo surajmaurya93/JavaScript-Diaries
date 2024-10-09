d8be88ab9ec1542bbcf4d5eef5f863d963dd3bee
Suraj Maurya <surajmaurya0706@gmail.com>
2024-10-10 01:33:21 +0530

    JavaScript

---
+++ Day 15 - HTML DOM In JavaScript/DOM Theory.js
@@ -0,0 +1,14 @@
+// window object --> global object(root element ke bhi upper sabse top heirarchy pe hota hai)
+// all variables all methods iske under hote hai window object(globel object)
+// in web pages we can open new window close new window all this is created by browser.
+//console.log is alsa window object --> window.console.log("Suraj Maurya") ==> //Output:- Suraj Maurya
+
+// DOM :- Document object Model
+// HTML code ko convert kar diya javascript ke object per usko Document object model Kahte Hai. 
+
+// Character + <> = Tags 
+// Tokeniser:- Tags Ko Tokens Me Convert Karta Hai And Then Node Me COnvert Karta Hai And Then Uske Baad Multiple Nodes Milke DOM Bante Hai 
+// DOM :- tree like structure
+
+// BOM:- Browser Object Model
+//jo bhi interaction browser ke saath ho rahi except the html concept1.
---
+++ Day 15 - HTML DOM In JavaScript/Script.js
@@ -0,0 +1,17 @@
+// let mydiv = document.querySelector("#mydiv")
+
+// let newElement = document.createElement ("span");
+
+// newElement.textContent = " THis is me suraj maurya"
+
+// mydiv.insertAdjacentElement("beforebegin", newElement)
+
+// let parent = document.querySelector("#mydiv");
+// let child = document.querySelector("#fpara");
+// parent.removeChild(child);
+
+
+let parent = document.querySelector("#mydiv");
+let child = document.querySelector("#Spara");
+parent.removeChild(child)
+
